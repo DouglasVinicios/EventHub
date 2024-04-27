8d7d35e8f9ea9f5f19dddbfb1d6a49436bf7c56b
Douglas VInicios <douglasvinicios270@gmail.com>
2024-04-27 18:58:59 -0300

    script para instalar as dependencias

---
+++ ~/npm-install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CONTAINER="eventhub-eventhub-1"
+
+# Verifica se o contêiner está em execução
+if [ "$(docker inspect -f '{{.State.Running}}' "$CONTAINER" 2>/dev/null)" = "true" ]; then
+    echo "Contêiner está em execução, executando npm install dentro do contêiner..."
+
+    # Executa o comando npm install dentro do contêiner
+    docker exec "$CONTAINER" sh -c 'cd /var/www/html && npm install -y --verbose --legacy-peer-deps'
+
+    echo "npm install concluído."
+
+    echo "Reiniciando o container "$CONTAINER""
+
+    docker restart $CONTAINER
+
+    echo "Container "$CONTAINER" reiniciado"
+else
+    echo "Erro: Contêiner "$CONTAINER" não está em execução."
+fi
