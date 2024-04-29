9ce42ac488ebbcde50a271b555a3953f45167db1
Douglas VInicios <douglasvinicios270@gmail.com>
2024-04-29 18:33:55 -0300

    dashboard inicial

---
+++ resources/views/dashboard/app.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('title', 'Inicio')
+
+@section('content')
+    <div class="row w-75 m-auto">
+        <div class="col-sm-12 mb-2">
+            <div class="card event-shadow event-focus">
+                <div class="card-header">
+                    <h5 class="card-title">Evento interessante</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">Descrição do evento</h6>
+                </div>
+                <div class="card-body overflow-auto">
+                    <p class="card-text fst-italic opacity-75">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="btn btn-sm btn-success">Saiba mais</a>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+@endsection
