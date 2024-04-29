7ce5db8d6b714f3d5195ea54e06b0967b37a7b4b
Douglas VInicios <douglasvinicios270@gmail.com>
2024-04-29 18:33:36 -0300

    Layout base, navbar e footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="bg-dark text-light fixed-bottom p-3">
+    <div class="d-flex justify-content-evenly">
+        <p>Author: Douglas Vinicios</p>
+        <address>
+            <p><a href="mailto:#">exemple@exemple.com</a></p>
+        </address>
+    </div>
+</footer>
