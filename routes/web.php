b563d41a396b1e226e8d6c5c03ec96b3aa2b452d
Douglas VInicios <douglasvinicios270@gmail.com>
2024-04-30 20:12:50 -0300

    Usando o controller de eventos

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
+
+Route::get('/events', [EventController::class, 'index']);
