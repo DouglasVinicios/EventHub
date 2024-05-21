a4cb4475b82aa1970fc3532f88b12124ffcba81b
Douglas VInicios <douglasvinicios270@gmail.com>
2024-05-21 14:55:02 -0300

    Pagina de inicio

--- resources/views/dashboard/app.blade.php
+++ resources/views/dashboard/app.blade.php
@@ -3,9 +3,7 @@
 @section('title', 'Inicio')
 
 @section('content')
-    <div class="row w-75 m-auto">
-        <div class="col-sm-12 mb-2">
-            <x-event/>
-        </div>
+    <div class="row row-gap-3 w-75 m-auto my-3">
+        <div class="col-sm-12 mb-2"></div>
     </div>
 @endsection
